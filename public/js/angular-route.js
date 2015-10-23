7db7c44163a06d3d5a9c39c7f4d490a58c930e60
Student <student@RU-022.local>
2015-10-23 00:04:50 -0600

    included Chart JS

--- public/js/angular-route.js
+++ public/js/angular-route.js
@@ -1,4 +1,5 @@
-angular.module("demoApp",["ngRoute"])
+// added chart.js dependency
+angular.module("demoApp",["ngRoute", "chart.js", "ngMaterial"])
 
 angular.module("demoApp").config(["$routeProvider",function($routeProvider){
 
@@ -11,4 +12,18 @@ angular.module("demoApp").config(["$routeProvider",function($routeProvider){
 
 angular.module("demoApp").controller("demoController",["$scope","$http",function($scope,$http){
 
+			// Ed's Angular stuff
+			$scope.greeting = 'Surprise Surprise'
+			
+			 
+
+			$scope.labels = ["Blue", "Grey", "Red"];
+  			$scope.data = [100,100,100]
+  			
+
+
+  			//end of Ed's Angular Stuff
+
 }])
+
+
